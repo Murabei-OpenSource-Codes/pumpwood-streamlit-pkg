f3eea602a8bad86461b0d5812ab2cb4079725680
Lucas Ribeiro Magalhaes <lucas.magalhaes@murabei.com>
2026-08-18 18:08:42 -0300

    Update build process to use Poetry, add versioning for non-main branches, and create pyproject.toml from template

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# Update version path
 VERSION_FILE="VERSION"
 current_version=$(grep -E "^VERSION=" "$VERSION_FILE" | cut -d'=' -f2)
 IFS='.' read -r major minor patch <<< "$current_version"
@@ -7,12 +8,17 @@ sed -i "s/^VERSION=.*/VERSION=$new_version/" "$VERSION_FILE"
 echo "Updated version to $new_version"
 
 source VERSION
-sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
+
+# Detecta branch atual e adiciona -beta se não for main/master
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$CURRENT_BRANCH" != "main" && "$CURRENT_BRANCH" != "master" ]]; then
+  VERSION="${VERSION}-b.0"
+fi
+
+sed -e 's#{VERSION}#'"${VERSION}"'#g' pyproject_template.toml > pyproject.toml
 
 rm -R build/
-python3 setup.py build sdist bdist_wheel
-
-pdoc --docformat="google" src/pumpwood_streamlit -o ./docs
+poetry build
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
